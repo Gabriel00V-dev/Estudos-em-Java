5ded06b9cb4809439cdd897c3e37a7a724efffe9
Gabriel Vasconcelos <gabriel00souzavasconcelos@gmail.com>
2026-05-20 18:20:45 -0300

    ReformulaçãoNomesArquivosEDA04

--- 02- Estrutura de Dados e Algoritmo/01_Arrays/arrays04/MainArray_S02L04.java
+++ 02- Estrutura de Dados e Algoritmo/01_Arrays/arrays04/MainArray_S02L04.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class MainArray_S02L04{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int[] lista = new int[5];
-
-        for(int i = 0; i < lista.length; i++){
-            System.out.println("Digite um numero inteiro: ");
-            lista[i] = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            for(int i = 0; i < lista.length; i++){
+                System.out.println("Digite um numero inteiro: ");
+                lista[i] = sc.nextInt();
+            }
         }
-        
+
         for(int i = 0; i < lista.length; i++){ //Aplicando e entendendo BubbleSort
             for(int j = 0; j < (lista.length - 1); j++){
                 if(lista[j] > lista[j+1]){
