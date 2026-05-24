5e2ba5451a035d9de9219551848387e9e1edda9e
Gabriel Vasconcelos <gabriel00souzavasconcelos@gmail.com>
2026-05-24 11:21:41 -0300

    Update MainArrayList_S02L07

---
+++ 02- Estrutura de Dados e Algoritmo/03_ArrayList/arraylist07/MainArrayList_S02L07.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class MainArrayList_S02L07 {
+    public static void main(String[] args) {
+        ArrayList <Integer> n1 = new ArrayList<>();
+        
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < 7; i++){
+                System.out.println("Digite um numero");
+                n1.add(sc.nextInt());
+            }
+
+            for(int i = 0; i < n1.size()-1; i++){
+                int min = i;
+
+                for(int j = i+1; j < n1.size(); j++){
+                    if(n1.get(j) < n1.get(min)){
+                        min = j;
+                    }
+                }
+                int temp = n1.get(min);
+                n1.set(min, n1.get(i));
+                n1.set(i, temp);
+            }
+            System.out.println("Ordenado: " + n1);
+    }
+}
