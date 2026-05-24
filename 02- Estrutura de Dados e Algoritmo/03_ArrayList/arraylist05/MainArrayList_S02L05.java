9eb1db1443fdacc8052a8f0a0a95d232fe04ad14
Gabriel Vasconcelos <gabriel00souzavasconcelos@gmail.com>
2026-05-24 11:21:19 -0300

    Update MainArrayList_S02L05

---
+++ 02- Estrutura de Dados e Algoritmo/03_ArrayList/arraylist05/MainArrayList_S02L05.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class MainArrayList_S02L05 {
+    public static void main(String[] args) {
+        ArrayList <String> produtos = new ArrayList<>();
+
+        produtos.add("refrigerante");
+        produtos.add("chocolate");
+        produtos.add("fruta");
+        produtos.add("bolacha");
+
+        System.out.println("Digite o que deseja encontrar: ");
+        
+        Scanner sc = new Scanner(System.in);
+            String item = sc.nextLine();
+
+            boolean disponivel = produtos.contains(item);
+            int posicao = produtos.indexOf(item);
+            if(disponivel == true){
+                System.out.println("O item está disponível! Posição -> " + posicao);
+            }else{
+                System.out.println("O item não está disponível!");
+            }
+        
+    }
+}
