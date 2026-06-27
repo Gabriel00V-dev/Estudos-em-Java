a16b8fde29082a71d1d6013501eac3cf9eca115b
Gabriel Vasconcelos <gabriel00souzavasconcelos@gmail.com>
2026-06-27 16:40:46 -0300

    Update Hash_S02L03

---
+++ 02- Estrutura de Dados e Algoritmo/06_HashEquals/HashEquals03/PersonHash_S02L03.java
@@ -0,0 +1,50 @@
+public class PersonHash_S02L03 {
+    private int id;
+    private String name;
+    public PersonHash_S02L03(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        PersonHash_S02L03 other = (PersonHash_S02L03) obj;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+    @Override
+    public String toString() {
+        return "PersonHash_S02L04 [id=" + id + ", name=" + name + "]";
+    }
+}
