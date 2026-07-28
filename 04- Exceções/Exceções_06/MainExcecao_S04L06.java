47309050852eac083757a34cab54b571c8dda186
Gabriel Vasconcelos <gabriel00souzavasconcelos@gmail.com>
2026-07-27 21:19:28 -0300

    Update Excecao_S04L06

---
+++ 04- Exceções/Exceções_06/MainExcecao_S04L06.java
@@ -0,0 +1,28 @@
+public class MainExcecao_S04L06 {
+    public static void main(String[] args) {
+        abreConexao2();
+    }
+
+    private static String abreConexao(){
+        try{
+            System.out.println("Abrindo arquivo");
+            System.out.println("Escrevendo dados no arquivo");
+            return "conexão aberta";
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{ //Finally sempre sempre é executado
+            System.out.println("Fechando recurso liberado pelo SO");
+        }
+        return null;
+    }
+
+        private static String abreConexao2(){
+        try{
+            System.out.println("Abrindo arquivo");
+            System.out.println("Escrevendo dados no arquivo");
+            throw new RuntimeException();
+        }finally{ 
+            System.out.println("Fechando recurso liberado pelo SO");
+        }
+    }
+}
