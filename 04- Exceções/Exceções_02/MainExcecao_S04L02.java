c65483a5169fd2f4e35acc3e5d2dc3379b8baae3
Gabriel Vasconcelos <gabriel00souzavasconcelos@gmail.com>
2026-07-27 21:18:47 -0300

    Update Excecao_S04L02

---
+++ 04- Exceções/Exceções_02/MainExcecao_S04L02.java
@@ -0,0 +1,6 @@
+public class MainExcecao_S04L02 {
+    public static void main(String[] args) { //RunTimeExceptions
+        int[] nums = {1, 2};
+        System.out.println(nums[2]); //ArrayIndexOutOfBoundsException
+    }
+}
