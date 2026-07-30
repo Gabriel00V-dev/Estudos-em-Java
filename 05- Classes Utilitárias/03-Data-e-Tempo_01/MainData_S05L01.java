13fd85a2866b29d519a81d129336c1477e764dc1
Gabriel Vasconcelos <gabriel00souzavasconcelos@gmail.com>
2026-07-30 20:21:05 -0300

    Update Data_S05L01

---
+++ 05- Classes Utilitárias/03-Data-e-Tempo_01/MainData_S05L01.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+public class MainData_S05L01 {
+    public static void main(String[] args) {
+        Date date = new Date(); //long 1000000
+        System.out.println(date);
+    }
+}
