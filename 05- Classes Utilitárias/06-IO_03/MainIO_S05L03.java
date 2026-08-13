caab2043aa418022d88767c4bbd33faaceb9bc72
Gabriel Vasconcelos <gabriel00souzavasconcelos@gmail.com>
2026-08-12 21:33:17 -0300

    Update IO_S05L03

---
+++ 05- Classes Utilitárias/06-IO_03/MainIO_S05L03.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MainIO_S05L03 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try (FileReader fr = new FileReader(file)){
+        int i;
+            while ((i=fr.read()) != -1) {
+                System.out.print((char)i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
