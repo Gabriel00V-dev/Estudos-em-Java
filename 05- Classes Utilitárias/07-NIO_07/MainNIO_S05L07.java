de7cf7d643b8cd1eb3ecbe3da4e9c7e16fd67b71
Gabriel Vasconcelos <gabriel00souzavasconcelos@gmail.com>
2026-08-22 10:10:50 -0300

    Update NIO_S05L07

---
+++ 05- Classes Utilitárias/07-NIO_07/MainNIO_S05L07.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+
+public class MainNIO_S05L07 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("pasta2/new.txt");
+        BasicFileAttributes basicFileAttributes = Files.readAttributes(path, BasicFileAttributes.class);
+        FileTime creationTime = basicFileAttributes.creationTime();
+        FileTime lastModifiedTime = basicFileAttributes.lastModifiedTime();
+        FileTime lastAccesstiTime = basicFileAttributes.lastAccessTime();
+
+        System.out.println("CreationTime "+ creationTime);
+        System.out.println("LastModifiedTime "+ lastModifiedTime);
+        System.out.println("LastAccesstime "+ lastAccesstiTime);
+        System.out.println("---------------");
+
+        BasicFileAttributeView fileAttributeView = Files.getFileAttributeView(path, BasicFileAttributeView.class);
+        FileTime newCreationTime = FileTime.fromMillis(System.currentTimeMillis());
+        fileAttributeView.setTimes(lastModifiedTime, newCreationTime, creationTime);
+
+        creationTime = fileAttributeView.readAttributes().creationTime();
+        lastModifiedTime = fileAttributeView.readAttributes().lastModifiedTime();
+        lastAccesstiTime = fileAttributeView.readAttributes().lastAccessTime();
+
+        System.out.println("CreationTime "+ creationTime);
+        System.out.println("LastModifiedTime "+ lastModifiedTime);
+        System.out.println("LastAccessTime "+ lastAccesstiTime);
+    }
+}
