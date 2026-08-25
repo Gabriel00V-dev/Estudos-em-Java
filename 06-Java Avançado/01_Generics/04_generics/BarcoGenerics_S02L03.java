a89138c34dbdcafd6506060cfcc40aaadc1e3f48
Gabriel Vasconcelos <gabriel00souzavasconcelos@gmail.com>
2026-08-25 18:09:34 -0300

    Update Generics_S05L03

---
+++ 06-Java Avançado/01_Generics/04_generics/BarcoGenerics_S02L03.java
@@ -0,0 +1,13 @@
+public class BarcoGenerics_S02L03 {
+    private String nome;
+
+    public BarcoGenerics_S02L03(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "BarcoGenerics_S02L03 [nome=" + nome + "]";
+    }
+
+}
